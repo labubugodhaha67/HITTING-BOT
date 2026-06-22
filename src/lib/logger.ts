d40c27325ab4b8708712a4858cdb5f8b5f56dcaa
labubugodhaha67 <labubu6767167@outlook.com>
2026-06-22 08:05:24 +0100

    Update logger.ts

--- src/lib/logger.ts
+++ src/lib/logger.ts
@@ -1,1 +1,5 @@
-import { logger } from "./lib/logger";
+export const logger = {
+  info: (...args: any[]) => console.log(...args),
+  warn: (...args: any[]) => console.warn(...args),
+  error: (...args: any[]) => console.error(...args),
+};
