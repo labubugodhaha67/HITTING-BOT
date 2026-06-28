b532cf148bf4853e09254812ee63999f4cdf9585
labubugodhaha67 <labubu6767167@outlook.com>
2026-06-28 07:17:47 +0100

    Delete src directory

--- src/lib/logger.ts
+++
@@ -1,5 +0,0 @@
-export const logger = {
-  info: (...args: any[]) => console.log(...args),
-  warn: (...args: any[]) => console.warn(...args),
-  error: (...args: any[]) => console.error(...args),
-};
